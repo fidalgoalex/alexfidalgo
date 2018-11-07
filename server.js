2251a7dfacd7659ab797168032a0650acc2b1bd1
fidalgoalex <fidalgo.alex1@gmail.com>
2018-11-07 12:06:08 -0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res)=>{
     res.status(200).send({ messange: "Ola mundo estou funcionando"});
 })
 
+app.get('/ola', (req, res)=>{
+    res.status(200).send({ messange: "Oi eu sou a rota ola"});
+})
+
 app.listen(3001, () =>{
     console.log('Api rodando na porta 3001');
 })
